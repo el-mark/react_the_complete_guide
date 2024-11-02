26c9bc077d28dd1056a59c6ed6894bd7922000fa
Mark Harmsen <mharmsenr@gmail.com>
2024-11-02 16:57:02 -0500

    TabButton gets a onclick function

--- 3_react_essentials/1_starting_project/src/components/TabButton.jsx
+++ 3_react_essentials/1_starting_project/src/components/TabButton.jsx
@@ -1,3 +1,11 @@
 export default function TabButton({children}) {
-    return <li><button>{children}</button></li>;
+    function handleClick() {
+        console.log('hola');
+    }
+
+    return (
+        <li>
+            <button onClick={handleClick}>{children}</button>
+        </li>
+    );
 }
