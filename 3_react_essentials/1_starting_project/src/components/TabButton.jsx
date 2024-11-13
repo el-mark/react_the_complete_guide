bfeca6995bc7c4de7628fd3f7e5920bae224503d
Mark Harmsen <mharmsenr@gmail.com>
2024-11-13 11:28:27 -0500

    dynamic class bind to selectedTopic

--- 3_react_essentials/1_starting_project/src/components/TabButton.jsx
+++ 3_react_essentials/1_starting_project/src/components/TabButton.jsx
@@ -1,7 +1,9 @@
-export default function TabButton({children, onSelect}) {
+export default function TabButton({children, onSelect, isSelected}) {
     return (
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button onClick={onSelect} className={isSelected ? 'active' : ''}>
+                {children}
+            </button>
         </li>
     );
 }
