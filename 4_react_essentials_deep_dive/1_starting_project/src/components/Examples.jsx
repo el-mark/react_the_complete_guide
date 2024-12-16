44f53186997ef5a4e27477e0c5d996855a3cdd1c
Mark Harmsen <mharmsenr@gmail.com>
2024-12-15 23:05:50 -0500

    exercise_14

--- 4_react_essentials_deep_dive/1_starting_project/src/components/Examples.jsx
+++ 4_react_essentials_deep_dive/1_starting_project/src/components/Examples.jsx
@@ -42,7 +42,7 @@ export default function Examples() {
         
         {!selectedTopic ? (
             <p>Please select a topic.</p>
-            ) : (
+          ) : (
             <div id="tab-content">
                 <h3>{EXAMPLES[selectedTopic].title}</h3>
                 <p>{EXAMPLES[selectedTopic].description}</p>
