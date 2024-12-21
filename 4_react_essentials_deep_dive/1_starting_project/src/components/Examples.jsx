5cec3bc782527d53bc61a0f42cd3d80b00d16c16
Mark Harmsen <mharmsenr@gmail.com>
2024-12-20 19:38:55 -0500

    ButtonsContainer, which is the wrapper, as a prop

--- 4_react_essentials_deep_dive/1_starting_project/src/components/Examples.jsx
+++ 4_react_essentials_deep_dive/1_starting_project/src/components/Examples.jsx
@@ -17,6 +17,7 @@ export default function Examples() {
     return (
       <Section id="examples" title="Examples">
         <Tabs
+          ButtonsContainer="menu"
           buttons={
             <>
               <TabButton
