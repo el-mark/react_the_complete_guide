bf76a270e488c55604dd42846f9ea0a9c1d2105d
Mark Harmsen <mharmsenr@gmail.com>
2025-01-21 16:05:33 -0500

    lift the state up

--- 4_react_essentials_deep_dive/7_tic-tac-toe-starting-project/src/components/Player.jsx
+++ 4_react_essentials_deep_dive/7_tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Player({initialName, symbol}) {
+export default function Player({initialName, symbol, isActive}) {
     const [playerName, setPlayerName] = useState(initialName);
     const [isEditing, setIsEditing] = useState(false);
 
@@ -18,7 +18,7 @@ export default function Player({initialName, symbol}) {
         editablePlayerName = <input type="text" value={playerName} onChange={handleChange} required/>;
     }
 
-    return <li>
+    return <li className={isActive ? 'active' : undefined}>
         <span className="player">
             {editablePlayerName}
             <span className="player-symbol">{symbol}</span>
