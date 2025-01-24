12c0f693273f7aa89da25a537103173ad44b84eb
Mark Harmsen <mharmsenr@gmail.com>
2025-01-24 16:44:51 -0500

    lift up state and create results component

--- 5_react_essentials_practive_project/5_2_course_solution/src/App.jsx
+++ 5_react_essentials_practive_project/5_2_course_solution/src/App.jsx
@@ -1,66 +1,29 @@
 import { useState } from 'react';
-import { formatter } from './util/investment';
-import { calculateInvestmentResults } from './util/investment';
 import Header from './components/Header';
 import UserInput from './components/UserInput';
+import Results from './components/Results';
 
 function App() {
-  const [ initialInvestment, setInitialInvestment] = useState(10000);
-  function handleInvestmentChange(event) {
-    setInitialInvestment(event.target.value);
-  }
-
-  const [ annualInvestment, setAnnualInvestment] = useState(1200);
-  function handleAnnualInvestmentChange(event) {
-    setAnnualInvestment(event.target.value);
-  }
-
-  const [ expectedReturn, setExpectedReturn] = useState(6);
-  function handleExpectedReturnChange(event) {
-    setExpectedReturn(event.target.value);
-  }
-
-  const [ duration, setDuration] = useState(10);
-  function handleDurationChange(event) {
-    setDuration(event.target.value);
-  }
-
-  const tableRows = calculateInvestmentResults({
-    initialInvestment, annualInvestment, expectedReturn, duration
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
   });
 
-  let totalInterest = 0;
-  let totalAnnualInvestment = 0;
+  function handleChange(inputIdentifier, newValue) {
+      setUserInput(prevUserInput => (
+          {...prevUserInput, [inputIdentifier]: newValue}
+      ));
+  }
+
+  // let totalInterest = 0;
+  // let totalAnnualInvestment = 0;
 
   return (<div>
     <Header></Header>
-    <UserInput></UserInput>
-    <main>
-      <div id="result">
-        <table>
-          <thead>
-            <tr>
-              <th>Year</th>
-              <th>Investment Value</th>
-              <th>Interest (Year)</th>
-              <th>Total Interest</th>
-              <th>Invested Capital</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableRows.map((row, rowIndex) => (
-              <tr key={rowIndex}>
-                <td>{formatter.format(row.year)}</td>
-                <td>{formatter.format(row.valueEndOfYear)}</td>
-                <td>{formatter.format(row.interest)}</td>
-                <td>{formatter.format(totalInterest += row.interest)}</td>
-                <td>{formatter.format(totalAnnualInvestment += row.annualInvestment)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </main>
+    <UserInput userInput={userInput} handleChange={handleChange}></UserInput>
+    <Results userInput={userInput} ></Results>
   </div>)
 }
 
---
+++ 5_react_essentials_practive_project/5_2_course_solution/src/components/Results.jsx
@@ -0,0 +1,35 @@
+import { formatter } from '../util/investment';
+import { calculateInvestmentResults } from '../util/investment';
+
+export default function Results({userInput}) {
+    const tableRows = calculateInvestmentResults(userInput);
+
+    return (
+        <main>
+            <div id="result">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Year</th>
+                        <th>Investment Value</th>
+                        <th>Interest (Year)</th>
+                        <th>Total Interest</th>
+                        <th>Invested Capital</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableRows.map((row, rowIndex) => (
+                        <tr key={rowIndex}>
+                        <td>{formatter.format(row.year)}</td>
+                        <td>{formatter.format(row.valueEndOfYear)}</td>
+                        <td>{formatter.format(row.interest)}</td>
+                        <td>{formatter.format(row.interest)}</td>
+                        <td>{formatter.format(row.annualInvestment)}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            </div>
+        </main>
+    )
+}
