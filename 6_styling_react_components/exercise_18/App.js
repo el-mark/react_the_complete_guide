0735790beca858bf5e8f4639c384ef3bf9155274
Mark Harmsen <mharmsenr@gmail.com>
2025-01-27 14:45:25 -0500

    exercise 18. Part 2

--- 6_styling_react_components/exercise_18/App.js
+++ 6_styling_react_components/exercise_18/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 
 function App() {
-    const [buttonState, setButtonSate] = React.useState('white');
-    
-    function hanldeOnClick(color) {
-        setButtonSate(color);
-    }
-    
-    return (
-    <div id="app">
-      <h1
-        style={{ color: buttonState }}
-      >CSS is great!</h1>
-      <menu>
-        <li>
-          <button onClick={() => hanldeOnClick('green')}>Yes</button>
-        </li>
-        <li>
-          <button onClick={() => hanldeOnClick('red')}>No</button>
-        </li>
-      </menu>
-    </div>
+  const [buttonState, setButtonSate] = React.useState('');
+  
+  function hanldeOnClick(color) {
+    setButtonSate(`highlight-${color}`);
+  }
+  
+  return (
+      <div id="app">
+        <h1
+          className={buttonState}
+        >
+          CSS is great!
+        </h1>
+        <menu>
+          <li>
+            <button onClick={() => hanldeOnClick('green')}>Yes</button>
+          </li>
+          <li>
+            <button onClick={() => hanldeOnClick('red')}>No</button>
+          </li>
+        </menu>
+      </div>
   );
 }
 
